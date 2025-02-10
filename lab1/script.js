330bea2aa3e84e6919525766186d857b7d089008
hronikiz <dolbaniob@gmail.com>
2025-02-10 09:15:48 +0200

    Update script.js

--- lab1/script.js
+++ lab1/script.js
@@ -1,1 +1,18 @@
+let name = 'Ignat';
+let birthYear = 18;
+let isStudent = true;
+Boolean(isStudent)
+console.log(name, birthYear, isStudent);
 
+let score = prompt("Введите ваш балл:");
+   if (score >= 90) {
+     console.log("Отлично!");
+   } else if (score >= 70) {
+     console.log("Хорошо");
+   } else {
+     console.log("Можно лучше!");
+   }
+
+   for (let i = 1; i <= 5; i++) {
+     console.log(`Итерация: ${i}`);
+   }
